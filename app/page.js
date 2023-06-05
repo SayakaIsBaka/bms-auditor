41cfe94a13fd3647f655943489d63b28e177ecfc
Sayaka <alice@lolita.technology>
2023-06-06 00:49:32 +0200

    changed page layout

--- app/page.js
+++ app/page.js
@@ -1,77 +1,77 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
-import { openDirectory } from './_lib/folder_picker';
-import { processFolder } from './_lib/process';
-import ResultsTable from './results';
-import { useState } from 'react';
+import Image from "next/image";
+import { openDirectory } from "./_lib/folder_picker";
+import { processFolder } from "./_lib/process";
+import ResultsTable from "./results";
+import { useState } from "react";
 
 export default function Home() {
   const [results, setResults] = useState();
 
   const loadFolder = async () => {
     let folder = await openDirectory();
-    if (folder === undefined)
-      return;
+    if (folder === undefined) return;
     // Filter subfolders
-    folder = folder.filter(e => e.webkitRelativePath.split('/').length === 2);
+    folder = folder.filter((e) => e.webkitRelativePath.split("/").length === 2);
     let res = await processFolder(folder);
     if (res !== null) {
       // display results
-      setResults((
-        <ResultsTable results={res} />
-      ));
+      setResults(<ResultsTable results={res} />);
     }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <>
+      <main className="flex flex-col h-screen items-center justify-center p-16">
+        <div className="relative flex place-items-center content-center">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
+            onClick={loadFolder}
+            style={{ cursor: "pointer" }}
             rel="noopener noreferrer"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Load{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Select the BMS folder to analyze.
+            </p>
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center">
-      <a
-          onClick={loadFolder}
-          style={{cursor: "pointer"}}
-          rel="noopener noreferrer"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Load{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        {results ? (
+        <div className="mb-32 border-gray-300 bg-gray-100 px-5 py-4 rounded-lg border border-transparent mt-8">
+          {results}
+        </div>
+        ) : "" }
+      <footer className="">
+          <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 text-xs pr-2">
+              BMS Auditor v0.0.1
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Select the BMS folder to analyze.
-          </p>
-        </a>
-      </div>
-      <div className="mb-32 border-gray-300 bg-gray-100 px-5 py-4 rounded-lg border border-transparent">
-        {results}
-      </div>
-    </main>
-  )
+            <a
+              href="https://github.com/SayakaIsBaka/bms-auditor"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </footer>
+      </main>
+    </>
+  );
 }
